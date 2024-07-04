07db3fd814190e21612888d1212b5db368ccc3bc
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-07-04 12:14:51 +0530

    Time: 24 ms (98.32%) | Memory: 40.4 MB (18.64%) - LeetSync

--- 278-first-bad-version/first-bad-version.java
+++ 278-first-bad-version/first-bad-version.java
@@ -1,24 +1,45 @@
+// /* The isBadVersion API is defined in the parent class VersionControl.
+//       boolean isBadVersion(int version); */
+
+// public class Solution extends VersionControl {
+//     public int firstBadVersion(int n) {
+        
+
+//         int low = 1;
+//         int high = n;
+//         int ans = 0;
+//         while(low<=high){
+//             int mid = low +(high-low)/2;
+//             if(isBadVersion(mid) && isBadVersion(mid-1)==false){
+//                 ans = mid;
+//                 break;
+//             }else if(isBadVersion(mid) && isBadVersion(mid-1)){
+//                 high = mid-1;
+//             }else{
+//                 low = mid+1;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        
-
         int low = 1;
         int high = n;
-        int ans = 0;
-        while(low<=high){
-            int mid = low +(high-low)/2;
-            if(isBadVersion(mid) && isBadVersion(mid-1)==false){
-                ans = mid;
-                break;
-            }else if(isBadVersion(mid) && isBadVersion(mid-1)){
-                high = mid-1;
-            }else{
-                low = mid+1;
+        
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (isBadVersion(mid)) {
+                high = mid; // Potential first bad version, continue searching in the left half
+            } else {
+                low = mid + 1; // First bad version must be after mid
             }
         }
-        return ans;
+        
+        return low; // low and high converge to the first bad version
     }
 }
