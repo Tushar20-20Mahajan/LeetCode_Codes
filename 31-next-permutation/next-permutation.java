09e707a633363312874a57be2fd936abb40aa485
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-07-07 18:54:05 +0530

    Time: 1 ms (20.70%) | Memory: 43.2 MB (14.67%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,75 +1,75 @@
-// class Solution {
-//     public void nextPermutation(int[] nums) {
-//         int pivot = -1;
-//         for(int i=1;i<nums.length;i++){
-//             if(nums[i]>nums[i-1]){
-//                 pivot = i-1;
-//                 break;
-//             }
-//         }
-//         if(pivot==-1){
-//             Arrays.sort(nums);
-//         }else{
-//             for(int i=nums.length-1;i>pivot;i--){
-//                 if(nums[i]>nums[pivot]){
-//                     int temp = nums[i];
-//                     nums[i]=nums[pivot];
-//                     nums[pivot]=temp;
-//                     break;
-//                 }
-//             }
-//             for(int i = pivot+1;i<nums.length;i++){
-//                 for(int j = i;j<nums.length;j++){
-//                     if(i!=j && nums[i]>nums[j]){
-//                         int temp = nums[i];
-//                         nums[i]=nums[j];
-//                         nums[j]=temp;
-//                     }
-//                 }
-//             }
-//         }   
-//     }
-// }
-
-import java.util.Arrays;
-
 class Solution {
     public void nextPermutation(int[] nums) {
         int pivot = -1;
-        // Find the rightmost pivot
         for (int i = nums.length - 1; i > 0; i--) {
             if (nums[i] > nums[i - 1]) {
                 pivot = i - 1;
                 break;
             }
         }
-
-        if (pivot == -1) {
-            // If no pivot is found, sort the array to get the lowest permutation
+        if(pivot==-1){
             Arrays.sort(nums);
-        } else {
-            // Find the rightmost successor to the pivot
-            for (int i = nums.length - 1; i > pivot; i--) {
-                if (nums[i] > nums[pivot]) {
-                    // Swap the pivot and successor
+        }else{
+            for(int i=nums.length-1;i>pivot;i--){
+                if(nums[i]>nums[pivot]){
                     int temp = nums[i];
-                    nums[i] = nums[pivot];
-                    nums[pivot] = temp;
+                    nums[i]=nums[pivot];
+                    nums[pivot]=temp;
                     break;
                 }
             }
-            // Reverse the suffix
-            reverse(nums, pivot + 1, nums.length - 1);
-        }
-    }
-
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
-        }
+            for(int i = pivot+1;i<nums.length;i++){
+                for(int j = i;j<nums.length;j++){
+                    if(i!=j && nums[i]>nums[j]){
+                        int temp = nums[i];
+                        nums[i]=nums[j];
+                        nums[j]=temp;
+                    }
+                }
+            }
+        }   
     }
 }
+
+// import java.util.Arrays;
+
+// class Solution {
+//     public void nextPermutation(int[] nums) {
+//         int pivot = -1;
+//         // Find the rightmost pivot
+//         for (int i = nums.length - 1; i > 0; i--) {
+//             if (nums[i] > nums[i - 1]) {
+//                 pivot = i - 1;
+//                 break;
+//             }
+//         }
+
+//         if (pivot == -1) {
+//             // If no pivot is found, sort the array to get the lowest permutation
+//             Arrays.sort(nums);
+//         } else {
+//             // Find the rightmost successor to the pivot
+//             for (int i = nums.length - 1; i > pivot; i--) {
+//                 if (nums[i] > nums[pivot]) {
+//                     // Swap the pivot and successor
+//                     int temp = nums[i];
+//                     nums[i] = nums[pivot];
+//                     nums[pivot] = temp;
+//                     break;
+//                 }
+//             }
+//             // Reverse the suffix
+//             reverse(nums, pivot + 1, nums.length - 1);
+//         }
+//     }
+
+//     private void reverse(int[] nums, int start, int end) {
+//         while (start < end) {
+//             int temp = nums[start];
+//             nums[start] = nums[end];
+//             nums[end] = temp;
+//             start++;
+//             end--;
+//         }
+//     }
+// }
