3b2ece1abbc177cb429da31f354e08ac3a540f53
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-07-07 12:54:08 +0530

    Time: 1 ms (81.16%) | Memory: 45.3 MB (84.87%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,29 +1,65 @@
+// class Solution {
+//     public void setZeroes(int[][] matrix) {
+//         ArrayList<Integer> list1 = new ArrayList<>();
+//         ArrayList<Integer> list2 = new ArrayList<>();
+//         for(int i=0;i<matrix.length;i++){
+//             for(int j = 0;j<matrix[i].length;j++){
+//                 if(matrix[i][j]==0){
+//                     list1.add(i);
+//                     list2.add(j);
+                    
+//                 }
+//             }
+//         }
+//         for(int i=0;i<matrix.length;i++){
+//             for(int j = 0;j<matrix[i].length;j++){
+//                 if(list1.contains(i)){
+//                     matrix[i][j]=0;
+//                 }
+//                 if(list2.contains(j)){
+//                     matrix[i][j]=0;
+//                 }
+
+//             }
+//         }
+
+        
+        
+//     }
+// }
+
 class Solution {
     public void setZeroes(int[][] matrix) {
-        ArrayList<Integer> list1 = new ArrayList<>();
-        ArrayList<Integer> list2 = new ArrayList<>();
+        int col0 = 1;
         for(int i=0;i<matrix.length;i++){
             for(int j = 0;j<matrix[i].length;j++){
                 if(matrix[i][j]==0){
-                    list1.add(i);
-                    list2.add(j);
-                    
-                }
-            }
-        }
-        for(int i=0;i<matrix.length;i++){
-            for(int j = 0;j<matrix[i].length;j++){
-                if(list1.contains(i)){
-                    matrix[i][j]=0;
-                }
-                if(list2.contains(j)){
-                    matrix[i][j]=0;
+                    matrix[i][0]=0;
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }else{
+                        col0 =0;
+                    }
+
                 }
 
             }
         }
+        for(int i=1;i<matrix.length;i++){
+            for(int j = 1;j<matrix[i].length;j++){  
+                if((matrix[i][j]!=0)&&(matrix[0][j]==0 || matrix[i][0]==0)){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(matrix[0][0]==0){
+            Arrays.fill(matrix[0],0);
+        }
+        if(col0==0){
+            for(int i=0;i<matrix.length;i++){
+                matrix[i][0]=0;
+            }
+        }
 
-        
-        
     }
 }
