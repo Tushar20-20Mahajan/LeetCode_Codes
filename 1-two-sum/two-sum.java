9da2f5f2fc006e7363c95991285ac3e4ccdc59ed
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-07-07 16:55:52 +0530

    Time: 18 ms (52.02%) | Memory: 44.8 MB (49.35%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,17 +1,33 @@
+import java.util.ArrayList;
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Collections;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n=nums.length;
-        int [] array = new int[2];
-       for(int i=0;i<n;i++){
-           for(int j=0;j<n;j++){
-               if(i!=j && nums[i]+nums[j]==target){
-                   array[0]=i;
-                   array[1]=j;    
-               }
-           }
-       }
+        int [] arr = new int[2];
+        ArrayList<Map.Entry<Integer,Integer>> list = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            list.add(new AbstractMap.SimpleEntry(nums[i],i));
+        }
+        list.sort(Map.Entry.comparingByKey());
+        int lp =0;
+        int rp = nums.length-1;
+        while(lp<=rp){
+            if((list.get(lp).getKey()+list.get(rp).getKey())==target){
+                arr[0]=list.get(lp).getValue();
+                arr[1]=list.get(rp).getValue();
+                break;
+            }
+            else if(list.get(lp).getKey()+list.get(rp).getKey()>target){
+                rp--;
+            }
+            else if(list.get(lp).getKey()+list.get(rp).getKey()<target){
+                lp++;
+            }
+        }
 
-       return array;
+       return arr;
         
     }
 }
