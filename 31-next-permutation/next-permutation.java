f459eadeccde5b750a4f2b88be4a7f394605995d
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-07-07 19:04:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (94.57%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -8,7 +8,15 @@ class Solution {
             }
         }
         if(pivot==-1){
-            Arrays.sort(nums);
+            int lp = 0;
+            int rp = nums.length-1;
+            while(lp<rp){
+                int temp = nums[lp];
+                nums[lp]=nums[rp];
+                nums[rp]=temp;
+                lp++;
+                rp--;
+            }
         }else{
             for(int i=nums.length-1;i>pivot;i--){
                 if(nums[i]>nums[pivot]){
@@ -30,46 +38,3 @@ class Solution {
         }   
     }
 }
-
-// import java.util.Arrays;
-
-// class Solution {
-//     public void nextPermutation(int[] nums) {
-//         int pivot = -1;
-//         // Find the rightmost pivot
-//         for (int i = nums.length - 1; i > 0; i--) {
-//             if (nums[i] > nums[i - 1]) {
-//                 pivot = i - 1;
-//                 break;
-//             }
-//         }
-
-//         if (pivot == -1) {
-//             // If no pivot is found, sort the array to get the lowest permutation
-//             Arrays.sort(nums);
-//         } else {
-//             // Find the rightmost successor to the pivot
-//             for (int i = nums.length - 1; i > pivot; i--) {
-//                 if (nums[i] > nums[pivot]) {
-//                     // Swap the pivot and successor
-//                     int temp = nums[i];
-//                     nums[i] = nums[pivot];
-//                     nums[pivot] = temp;
-//                     break;
-//                 }
-//             }
-//             // Reverse the suffix
-//             reverse(nums, pivot + 1, nums.length - 1);
-//         }
-//     }
-
-//     private void reverse(int[] nums, int start, int end) {
-//         while (start < end) {
-//             int temp = nums[start];
-//             nums[start] = nums[end];
-//             nums[end] = temp;
-//             start++;
-//             end--;
-//         }
-//     }
-// }
