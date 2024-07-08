e3e81098e5cdbf24770d2ef7efd721286c3c2923
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-07-08 12:43:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (9.93%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,18 +1,45 @@
 // (Brute Force -> Using extra space and making a new 2Darray)
+// class Solution {
+//     public void rotate(int[][] matrix) {
+//         int arr2d[][]=new int[matrix.length][matrix.length];
+//         for(int i=0;i<matrix.length;i++){
+//             int k = matrix.length-1;
+//             for(int j=0;j<matrix.length;j++){
+//                 arr2d[i][j]=matrix[k][i];
+//                 k--;
+//             }
+//         }
+//         for(int i=0;i<matrix.length;i++){
+//             for(int j=0;j<matrix.length;j++){
+//                 matrix[i][j]=arr2d[i][j];
+//             }
+//         }
+//     }
+// }
+
+
+// Using Transpose and then reversig all the rows 
 class Solution {
     public void rotate(int[][] matrix) {
-        int arr2d[][]=new int[matrix.length][matrix.length];
-        for(int i=0;i<matrix.length;i++){
-            int k = matrix.length-1;
-            for(int j=0;j<matrix.length;j++){
-                arr2d[i][j]=matrix[k][i];
-                k--;
+        for(int i=0;i<matrix.length-1;i++){
+            for(int j =i+1;j<matrix.length;j++){
+                int temp = matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
             }
         }
+
         for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix.length;j++){
-                matrix[i][j]=arr2d[i][j];
+            int lp = 0;
+            int rp = matrix.length-1;
+            while(lp<rp){
+                int temp = matrix[i][lp];
+                matrix[i][lp]=matrix[i][rp];
+                matrix[i][rp]=temp;
+                lp++;
+                rp--;
             }
         }
+
     }
 }
