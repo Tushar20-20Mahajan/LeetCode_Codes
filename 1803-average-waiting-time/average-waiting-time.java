04bfb85553df8e9a5beb213caddea09a737295e0
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-07-09 11:44:29 +0530

    Time: 3 ms (93.06%) | Memory: 72.9 MB (24.49%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.java
+++ 1803-average-waiting-time/average-waiting-time.java
@@ -1,17 +1,3 @@
-// class Solution {
-//     public double averageWaitingTime(int[][] customers) {
-//         int time = customers[0][0];
-//         double totalWatingTime = 0.0;
-//         for(int i = 0;i<customers.length;i++){
-//             int timeToCompleteTheOrder = customers[i][1];
-//             time = time + timeToCompleteTheOrder;
-//             totalWatingTime += time - (double)customers[i][0];
-//         }
-//         return (totalWatingTime/customers.length);
-        
-//     }
-// }
-
 class Solution {
     public double averageWaitingTime(int[][] customers) {
         int time = 0;
@@ -20,14 +6,8 @@ class Solution {
         for (int i = 0; i < customers.length; i++) {
             int arrivalTime = customers[i][0];
             int timeToCompleteOrder = customers[i][1];
-            
-            // Update the time to the maximum of current time and customer's arrival time
             time = Math.max(time, arrivalTime);
-            
-            // Chef starts cooking the order
             time += timeToCompleteOrder;
-            
-            // Calculate the waiting time for this customer
             totalWaitingTime += (time - arrivalTime);
         }
         
