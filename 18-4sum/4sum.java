16ada846b62322aafab683a6004c7e4cd55c8393
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-07-10 12:24:18 +0530

    Time: 18 ms (60.22%) | Memory: 44 MB (48.50%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,16 +1,55 @@
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     public List<List<Integer>> fourSum(int[] nums, int target) {
+//         Arrays.sort(nums);
+//         Set<List<Integer>> list = new HashSet<>();
+        
+//         if (nums.length < 4) {
+//             return new ArrayList<>(list);
+//         }
+        
+//         for (int i = 0; i < nums.length - 3; i++) {
+//             for (int j = i + 1; j < nums.length - 2; j++) {
+//                 int lp = j + 1;
+//                 int rp = nums.length - 1;
+//                 while (lp < rp) {
+//                     long sum = (long)nums[lp] + (long)nums[rp] + (long)nums[i] + (long)nums[j];
+//                     if (sum == target) {
+//                         List<Integer> quadruplet = Arrays.asList(nums[i], nums[j], nums[lp], nums[rp]);
+//                         list.add(quadruplet);
+//                         lp++;
+//                         rp--;
+//                     } else if (sum > target) {
+//                         rp--;
+//                     } else {
+//                         lp++;
+//                     }
+//                 }
+//             }
+//         }
+//         return new ArrayList<>(list);
+//     }
+// }
+
 
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
-        Set<List<Integer>> list = new HashSet<>();
+        List<List<Integer>> list = new ArrayList<>();
         
         if (nums.length < 4) {
-            return new ArrayList<>(list);
+            return list;
         }
         
         for (int i = 0; i < nums.length - 3; i++) {
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
             for (int j = i + 1; j < nums.length - 2; j++) {
+                if(j>i+1 && nums[j]==nums[j-1]){
+                    continue;
+                }
                 int lp = j + 1;
                 int rp = nums.length - 1;
                 while (lp < rp) {
@@ -18,7 +57,13 @@ class Solution {
                     if (sum == target) {
                         List<Integer> quadruplet = Arrays.asList(nums[i], nums[j], nums[lp], nums[rp]);
                         list.add(quadruplet);
+                        while(lp<rp && nums[lp]==nums[lp+1]){
+                            lp++;
+                        }
                         lp++;
+                         while(lp<rp && nums[rp]==nums[rp-1]){
+                            rp--;
+                        }
                         rp--;
                     } else if (sum > target) {
                         rp--;
@@ -28,6 +73,6 @@ class Solution {
                 }
             }
         }
-        return new ArrayList<>(list);
+        return list;
     }
 }
