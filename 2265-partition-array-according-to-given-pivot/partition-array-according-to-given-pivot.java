da95b541f0e7fd4e2f9742653d0545ec5037f276
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-07-10 22:50:47 +0530

    Time: 4 ms (100.00%) | Memory: 66.7 MB (59.34%) - LeetSync

---
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int arr[] = new int[nums.length];
+        int lp = 0;
+        int rp = nums.length - 1;
+        int right = nums.length - 1;
+        int count = 0;
+
+        // First pass: Place elements less than pivot in the correct position and count the pivot elements
+        for (int i : nums) {
+            if (i < pivot) {
+                arr[lp] = i;
+                lp++;
+            } else if (i == pivot) {
+                count++;
+            }
+        }
+
+        // Second pass: Place elements greater than pivot in the correct position
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] > pivot) {
+                arr[rp] = nums[i];
+                rp--;
+            }
+        }
+
+        // Place the pivot elements in between
+        while (count > 0) {
+            arr[lp] = pivot;
+            lp++;
+            count--;
+        }
+
+        return arr;
+    }
+}
