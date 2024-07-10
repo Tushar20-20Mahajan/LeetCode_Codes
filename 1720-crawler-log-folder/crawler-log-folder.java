729b8fa834541bec17007b99e55649ee6eac7e6c
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-07-10 12:01:38 +0530

    Time: 1 ms (99.42%) | Memory: 42.1 MB (37.73%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.java
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -1,17 +1,35 @@
+// class Solution {
+//     public int minOperations(String[] logs) {
+//         Stack<String> list = new Stack<>();
+//         for(int i=0;i<logs.length;i++){
+//             if(!list.isEmpty() && logs[i].equals("../")){
+//                 list.pop();
+//             }else if(logs[i].equals("./")){
+//                 continue;
+//             }else{
+//                 if(!logs[i].equals("../")){
+//                     list.push(logs[i]);
+//                 }
+//             }
+//         }
+//         return list.size();
+//     }
+// }
+
 class Solution {
     public int minOperations(String[] logs) {
-        Stack<String> list = new Stack<>();
-        for(int i=0;i<logs.length;i++){
-            if(!list.isEmpty() && logs[i].equals("../")){
-                list.pop();
-            }else if(logs[i].equals("./")){
-                continue;
-            }else{
-                if(!logs[i].equals("../")){
-                    list.push(logs[i]);
+        int res = 0;
+
+        for (String log : logs) {
+            if (log.equals("../")) {
+                if (res > 0) {
+                    res--;
                 }
+            } else if (!log.equals("./")) {
+                res++;
             }
         }
-        return list.size();
+
+        return res;
     }
 }
