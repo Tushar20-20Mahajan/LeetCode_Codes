138595148a04db4fc84846280adafcfedf7c7276
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-07-10 22:23:39 +0530

    Time: 4 ms (53.08%) | Memory: 85.2 MB (9.57%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,30 +1,49 @@
+
+// class Solution {
+//     public int[] rearrangeArray(int[] nums) {
+//         int[] positive = new int[(nums.length + 1) / 2];
+//         int[] negative = new int[(nums.length + 1) / 2];
+//         int x = 0;
+//         int y = 0;
+
+//         // Separate positive and negative numbers
+//         for (int i : nums) {
+//             if (i < 0) {
+//                 negative[y++] = i;
+//             } else {
+//                 positive[x++] = i;
+//             }
+//         }
+
+//         x = 0;
+//         y = 0;
+
+//         // Merge positive and negative numbers back into nums array alternately
+//         for (int i = 0; i < nums.length; i += 2) {
+//             nums[i] = positive[x++];
+//             if (i + 1 < nums.length) {
+//                 nums[i + 1] = negative[y++];
+//             }
+//         }
+
+//         return nums;
+//     }
+// }
+
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int[] positive = new int[(nums.length + 1) / 2];
-        int[] negative = new int[(nums.length + 1) / 2];
-        int x = 0;
-        int y = 0;
-
-        // Separate positive and negative numbers
-        for (int i : nums) {
-            if (i < 0) {
-                negative[y++] = i;
-            } else {
-                positive[x++] = i;
+        int [] arr = new int[nums.length];
+        int i=0;
+        int j =1;
+        for(int k:nums){
+            if(k>0){
+                arr[i]=k;
+                i=i+2;
+            }else{
+                arr[j]=k;
+                j=j+2;
             }
         }
-
-        x = 0;
-        y = 0;
-
-        // Merge positive and negative numbers back into nums array alternately
-        for (int i = 0; i < nums.length; i += 2) {
-            nums[i] = positive[x++];
-            if (i + 1 < nums.length) {
-                nums[i + 1] = negative[y++];
-            }
-        }
-
-        return nums;
+        return arr;
     }
 }
