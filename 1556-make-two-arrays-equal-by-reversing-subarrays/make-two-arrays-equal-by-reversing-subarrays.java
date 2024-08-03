d3f25f692a6004815752674de07cb9fa6a5d1e1f
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-08-04 00:25:58 +0530

    Time: 4 ms (54.32%) | Memory: 44.1 MB (56.26%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,20 +1,25 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        ArrayList<Integer> list = new ArrayList<>();
-        if(target.length!=arr.length){
-            return false;
-        }
-        for(int i=0;i<target.length;i++){
-            list.add(target[i]);
-        }
+        // ArrayList<Integer> list = new ArrayList<>();
+        // if(target.length!=arr.length){
+        //     return false;
+        // }
+        // for(int i=0;i<target.length;i++){
+        //     list.add(target[i]);
+        // }
 
-        for(int i=0;i<arr.length;i++){
-            if(!list.contains(arr[i])){
-                return false;
-            }else{
-                list.remove(Integer.valueOf(arr[i]));
-            }
+        // for(int i=0;i<arr.length;i++){
+        //     if(!list.contains(arr[i])){
+        //         return false;
+        //     }else{
+        //         list.remove(Integer.valueOf(arr[i]));
+        //     }
+        // }
+        Arrays.sort(target);
+        Arrays.sort(arr);
+        if(Arrays.equals(target,arr)){
+            return true;
         }
-        return true;
+        return false;
     }
 }
