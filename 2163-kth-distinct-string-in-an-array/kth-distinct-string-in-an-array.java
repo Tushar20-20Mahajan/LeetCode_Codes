56481d2c5e907c822337773df99ff40be6977409
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-08-05 18:38:31 +0530

    Time: 7 ms (55.29%) | Memory: 44.5 MB (17.65%) - LeetSync

--- 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.java
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.java
@@ -4,11 +4,11 @@ class Solution {
         for(int i=0;i<arr.length;i++){
             list.put(arr[i],list.getOrDefault(arr[i],0)+1);
         }
-        // int count =0;
+        int count =0;
         for (String word : list.keySet()) {
             if (list.get(word) == 1) {
-                --k;
-                if (k==0) {
+                count++;
+                if (count == k) {
                     return word;
                 }
             }
