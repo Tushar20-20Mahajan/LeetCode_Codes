4edae151eacdb92587f7ee11f4df19a906174ed9
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-08-22 22:33:37 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (90.99%) - LeetSync

--- 476-number-complement/number-complement.java
+++ 476-number-complement/number-complement.java
@@ -1,14 +1,21 @@
+// class Solution {
+//     public int findComplement(int num) {
+//         String str = Integer.toString(num,2);
+//         String s = new String("");
+//         for(int i=0;i<str.length();i++){
+//             if(str.charAt(i)=='1'){
+//                 s+='0';
+//             }else if(str.charAt(i)=='0'){
+//                 s+='1';
+//             }
+//         }
+//         return Integer.parseInt(s,2);
+//     }
+// }
+
 class Solution {
     public int findComplement(int num) {
-        String str = Integer.toString(num,2);
-        String s = new String("");
-        for(int i=0;i<str.length();i++){
-            if(str.charAt(i)=='1'){
-                s+='0';
-            }else if(str.charAt(i)=='0'){
-                s+='1';
-            }
-        }
-        return Integer.parseInt(s,2);
+        int mask = (Integer.highestOneBit(num) << 1) - 1;
+        return ~num & mask;
     }
 }
