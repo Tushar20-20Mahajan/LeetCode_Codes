4266c22f34cb5593da095cc1fb2add132b8118c3
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-09-03 23:12:24 +0530

    Time: 2 ms (71.59%) | Memory: 61.9 MB (27.33%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,20 +1,38 @@
 
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int minPrice = Integer.MAX_VALUE;
+//         int maxProfit = 0;
+        
+//         for (int price : prices) {
+//             // Update the minimum price encountered so far
+//             if (price < minPrice) {
+//                 minPrice = price;
+//             }
+//             // Calculate the profit if we sell at the current price
+//             else if (price - minPrice > maxProfit) {
+//                 maxProfit = price - minPrice;
+//             }
+//         }
+        
+//         return maxProfit;
+//     }
+// }
+
+
+
 class Solution {
     public int maxProfit(int[] prices) {
-        int minPrice = Integer.MAX_VALUE;
+        int minValue = prices[0];
         int maxProfit = 0;
-        
-        for (int price : prices) {
-            // Update the minimum price encountered so far
-            if (price < minPrice) {
-                minPrice = price;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<minValue){
+                minValue=prices[i];
             }
-            // Calculate the profit if we sell at the current price
-            else if (price - minPrice > maxProfit) {
-                maxProfit = price - minPrice;
+            else if (prices[i]-minValue > maxProfit){
+                maxProfit = prices[i]-minValue;
             }
         }
-        
         return maxProfit;
     }
 }
