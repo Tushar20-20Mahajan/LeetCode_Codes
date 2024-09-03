11062b5a160a6a582a462eec28a45283d6468cec
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-09-04 00:13:53 +0530

    Time: 1 ms (99.75%) | Memory: 55.3 MB (57.33%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,73 +1,73 @@
 // Using Division
 
-class Solution {
-
-    public int[] productExceptSelf(int[] nums) {
-        int product = 1;
-        boolean isZeroFound = false;
-        int zeroCount = 0;
-        boolean isConstantFound = false;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                isZeroFound = true;
-                zeroCount++;
-                continue;
-            }
-
-            if (nums[i] != 0) {
-                isConstantFound = true;
-            }
-            product *= nums[i];
-        }
-
-        int[] arr = new int[nums.length];
-
-        if (zeroCount > 1) {
-            Arrays.fill(arr, 0);
-        } else {
-            for (int i = 0; i < nums.length; i++) {
-                if (isZeroFound) {
-                    if (isConstantFound) {
-                        if (nums[i] == 0) {
-                            arr[i] = product;
-                        } else {
-                            arr[i] = 0;
-                        }
-                    } else {
-                        arr[i] = 0;
-                    }
-                } else {
-                    arr[i] = product / nums[i];
-                }
-            }
-        }
-
-        return arr;
-    }
-}
-
-// Using Surfix and Prefix
 // class Solution {
+
 //     public int[] productExceptSelf(int[] nums) {
-//         int n = nums.length;
-//         int[] result = new int[n];
-//         for (int i = 0; i < n; i++) {
-//             result[i] = 1;
+//         int product = 1;
+//         boolean isZeroFound = false;
+//         int zeroCount = 0;
+//         boolean isConstantFound = false;
+
+//         for (int i = 0; i < nums.length; i++) {
+//             if (nums[i] == 0) {
+//                 isZeroFound = true;
+//                 zeroCount++;
+//                 continue;
+//             }
+
+//             if (nums[i] != 0) {
+//                 isConstantFound = true;
+//             }
+//             product *= nums[i];
 //         }
-        
-//         int leftProduct = 1;
-//         for (int i = 0; i < n; i++) {
-//             result[i] *= leftProduct;
-//             leftProduct *= nums[i];
+
+//         int[] arr = new int[nums.length];
+
+//         if (zeroCount > 1) {
+//             Arrays.fill(arr, 0);
+//         } else {
+//             for (int i = 0; i < nums.length; i++) {
+//                 if (isZeroFound) {
+//                     if (isConstantFound) {
+//                         if (nums[i] == 0) {
+//                             arr[i] = product;
+//                         } else {
+//                             arr[i] = 0;
+//                         }
+//                     } else {
+//                         arr[i] = 0;
+//                     }
+//                 } else {
+//                     arr[i] = product / nums[i];
+//                 }
+//             }
 //         }
-        
-//         int rightProduct = 1;
-//         for (int i = n - 1; i >= 0; i--) {
-//             result[i] *= rightProduct;
-//             rightProduct *= nums[i];
-//         }
-        
-//         return result;
+
+//         return arr;
 //     }
 // }
+
+// Using Surfix and Prefix
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = 1;
+        }
+        
+        int leftProduct = 1;
+        for (int i = 0; i < n; i++) {
+            result[i] *= leftProduct;
+            leftProduct *= nums[i];
+        }
+        
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+        
+        return result;
+    }
+}
