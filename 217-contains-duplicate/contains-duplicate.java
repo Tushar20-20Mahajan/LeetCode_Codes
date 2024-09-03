a9a2341871328f894079593a14075de513a78c04
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-09-03 23:24:21 +0530

    Time: 11 ms (70.92%) | Memory: 58.8 MB (24.65%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,13 +1,26 @@
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         Arrays.sort(nums);
+//         if(nums.length==0 || nums.length==1){
+//             return false;
+//         }
+//         for(int i =1;i<nums.length;i++){
+//             if(nums[i]==nums[i-1]){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        if(nums.length==0 || nums.length==1){
-            return false;
-        }
-        for(int i =1;i<nums.length;i++){
-            if(nums[i]==nums[i-1]){
+        HashSet<Integer> list = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(list.contains(nums[i])){
                 return true;
             }
+            list.add(nums[i]);
         }
         return false;
     }
