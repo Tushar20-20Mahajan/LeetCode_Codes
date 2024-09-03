b735971a42e341ec163eb9e8d1d12edc5bcdf01b
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-09-04 00:20:23 +0530

    Time: 3 ms (23.57%) | Memory: 55 MB (96.15%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -48,26 +48,49 @@
 // }
 
 // Using Surfix and Prefix
+// class Solution {
+//     public int[] productExceptSelf(int[] nums) {
+//         int n = nums.length;
+//         int[] result = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             result[i] = 1;
+//         }
+        
+//         int leftProduct = 1;
+//         for (int i = 0; i < n; i++) {
+//             result[i] *= leftProduct;
+//             leftProduct *= nums[i];
+//         }
+        
+//         int rightProduct = 1;
+//         for (int i = n - 1; i >= 0; i--) {
+//             result[i] *= rightProduct;
+//             rightProduct *= nums[i];
+//         }
+        
+//         return result;
+//     }
+// }
+
+
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] result = new int[n];
-        for (int i = 0; i < n; i++) {
-            result[i] = 1;
-        }
+        int arr[] = new int[n];
+        Arrays.fill(arr,1);
         
         int leftProduct = 1;
-        for (int i = 0; i < n; i++) {
-            result[i] *= leftProduct;
-            leftProduct *= nums[i];
+        for(int i=0;i<nums.length;i++){
+            arr[i]*=leftProduct;
+            leftProduct*=nums[i];
         }
-        
-        int rightProduct = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            result[i] *= rightProduct;
-            rightProduct *= nums[i];
+
+        int rightProduct = 1 ;
+         for(int i=nums.length-1 ; i>=0;i--){
+            arr[i]*=rightProduct;
+            rightProduct*=nums[i];
         }
-        
-        return result;
+
+        return arr;
     }
 }
