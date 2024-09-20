9776438faa2465b72b22b134b9a7f65984c8e4d6
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-09-20 10:52:42 +0530

    Time: 5 ms (96.12%) | Memory: 41.3 MB (87.08%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,20 +1,41 @@
-class Solution {
+// class Solution {
 
-    public boolean isPalindrome(String s, int left, int right) {
-        while(left < right) {
-            if(s.charAt(left++) != s.charAt(right--)) return false;
-        } 
-        return true;
-    }
+//     public boolean isPalindrome(String s, int left, int right) {
+//         while(left < right) {
+//             if(s.charAt(left++) != s.charAt(right--)) return false;
+//         } 
+//         return true;
+//     }
     
+//     public int countSubstrings(String s) {
+//         int ans = 0;
+//         int n = s.length();
+//         for(int i=0;i<n;i++) {
+//             for(int j=i;j<n;j++) {
+//                 if(isPalindrome(s, i, j)) ans++;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+class Solution {
     public int countSubstrings(String s) {
-        int ans = 0;
         int n = s.length();
+        int ans = 0;
         for(int i=0;i<n;i++) {
-            for(int j=i;j<n;j++) {
-                if(isPalindrome(s, i, j)) ans++;
-            }
+            int even = palindromeCount(s, i, i+1);
+            int odd = palindromeCount(s, i-1, i+1);
+            ans += even + odd + 1;
         }
         return ans;
+    }
+
+    public int palindromeCount(String s, int left, int right) {
+        int count = 0;
+        while(left >= 0 && right < s.length() && s.charAt(left--) == s.charAt(right++)) {
+            count++;
+        }
+        return count;
     }
 }
