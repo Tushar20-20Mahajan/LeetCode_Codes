47f279a1b4231dd5496f161c019b51168c727644
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-09-24 01:56:09 +0530

    Time: 6 ms (69.68%) | Memory: 44.8 MB (35.00%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -20,54 +20,54 @@
 //     }
 // }
 
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         if (s == null || s.length() == 0) {
-//             return 0;
-//         }
-
-//         int maxLength = 0;
-//         int start = 0;
-//         int end = 0;
-//         Set<Character> set = new HashSet<>();
-
-//         while (end < s.length()) {
-//             if (!set.contains(s.charAt(end))) {
-//                 set.add(s.charAt(end));
-//                 end++;
-//                 maxLength = Math.max(maxLength, end - start);
-//             } else {
-//                 set.remove(s.charAt(start));
-//                 start++;
-//             }
-//         }
-
-//         return maxLength;
-//     }
-// }
-
-import java.util.Arrays;
-
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         if (s == null || s.length() == 0) {
             return 0;
         }
 
-        int[] count = new int[128]; 
-        int maxLen = 0, left = 0;
+        int maxLength = 0;
+        int start = 0;
+        int end = 0;
+        Set<Character> set = new HashSet<>();
 
-        for (int right = 0; right < s.length(); right++) {
-            count[s.charAt(right)]++;
-            
-            while (count[s.charAt(right)] > 1) {
-                count[s.charAt(left)]--;
-                left++;
+        while (end < s.length()) {
+            if (!set.contains(s.charAt(end))) {
+                set.add(s.charAt(end));
+                end++;
+                maxLength = Math.max(maxLength, end - start);
+            } else {
+                set.remove(s.charAt(start));
+                start++;
             }
-            
-            maxLen = Math.max(maxLen, right - left + 1);
         }
 
-        return maxLen;
+        return maxLength;
     }
 }
+
+// import java.util.Arrays;
+
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         if (s == null || s.length() == 0) {
+//             return 0;
+//         }
+
+//         int[] count = new int[128]; 
+//         int maxLen = 0, left = 0;
+
+//         for (int right = 0; right < s.length(); right++) {
+//             count[s.charAt(right)]++;
+            
+//             while (count[s.charAt(right)] > 1) {
+//                 count[s.charAt(left)]--;
+//                 left++;
+//             }
+            
+//             maxLen = Math.max(maxLen, right - left + 1);
+//         }
+
+//         return maxLen;
+//     }
+// }
