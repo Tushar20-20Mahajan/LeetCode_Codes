bcf351ca64d92f6a472e2dea8a5dd200303ff4e9
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-09-25 12:24:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (54.11%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -8,29 +8,40 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */ 
-class Solution {
-    public ListNode reverseList(ListNode head) {
-        ArrayList<Integer> list= new ArrayList<>();
-        ListNode temp = head;
-        while(temp!=null){
-            list.add(temp.val);
-            temp=temp.next;
-        }
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         ArrayList<Integer> list= new ArrayList<>();
+//         ListNode temp = head;
+//         while(temp!=null){
+//             list.add(temp.val);
+//             temp=temp.next;
+//         }
 
-        Collections.reverse(list);
+//         Collections.reverse(list);
 
-        ListNode dummy = new ListNode(0);
-        ListNode current=dummy;
-        for(int i=0;i<list.size();i++){
-            current.next= new ListNode(list.get(i));
-            current=current.next;
-        }
+//         ListNode dummy = new ListNode(0);
+//         ListNode current=dummy;
+//         for(int i=0;i<list.size();i++){
+//             current.next= new ListNode(list.get(i));
+//             current=current.next;
+//         }
 
-        ListNode abc=(dummy.next);
-        return abc;
+//         ListNode abc=(dummy.next);
+//         return abc;
 
         
+//     }
+// }
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode temp = null;
+        while(head != null){
+            ListNode next = head.next;
+            head.next = temp;
+            temp = head;
+            head = next;
+        }
+        return temp;
     }
 }
-
-
