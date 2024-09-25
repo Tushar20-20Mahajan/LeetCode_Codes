59135e22dfda28f5d47bebd2e8d5ea1d89488f85
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2024-09-25 12:53:14 +0530

    Time: 1 ms (99.88%) | Memory: 48.5 MB (58.51%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -9,23 +9,36 @@
  *     }
  * }
  */
-public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode s = headA;
-        ListNode f = headB;
-        while(s!=f){
-            if(s==null){
-                s = headA;
-            }else{
-                s=s.next;
-            }
-            if(f==null){
-                f = headB;
-            }else{
-                f=f.next;
-            }
+// public class Solution {
+//     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//         ListNode s = headA;
+//         ListNode f = headB;
+//         while(s!=f){
+//             if(s==null){
+//                 s = headA;
+//             }else{
+//                 s=s.next;
+//             }
+//             if(f==null){
+//                 f = headB;
+//             }else{
+//                 f=f.next;
+//             }
 
-        }
-        return s;
+//         }
+//         return s;
+//     }
+// }
+public class Solution {
+  public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    ListNode s = headA;
+    ListNode f = headB;
+    while (s != f) {
+      // If s reaches the end of listA, move it to headB
+      s = s == null ? headB : s.next;
+      // If f reaches the end of listB, move it to headA
+      f = f == null ? headA : f.next;
     }
+    return s; // At this point, either s and f are null (no intersection) or point to the intersection node
+  }
 }
