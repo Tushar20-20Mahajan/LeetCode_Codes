5984d120e08f8f8af0a1af56c3d552ed449d98df
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2025-07-06 16:43:37 +0530

    Time: 537 ms (31.90%) | Memory: 0B (100.00%) - LeetSync

---
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,13 @@
+-- /* Write your T-SQL query statement below */
+-- SELECT TOP(1) 
+-- customer_numer ,
+-- COUNT(customer_number) AS total_count
+-- FROM Orders
+-- GROUP BY customer_number
+-- ORDER BY total_count DESC
+
+SELECT TOP 1 
+    customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC;
